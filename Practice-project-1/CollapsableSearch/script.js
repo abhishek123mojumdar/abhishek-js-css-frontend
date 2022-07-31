1b0a10d70a9a0217f4e067f57f4bebd25b305f18
Mojumdar <Abhishek.Mojumdar@dell.com>
2022-07-31 17:50:13 +0530

    drag drop widget

--- Practice-project-1/CollapsableSearch/script.js
+++ Practice-project-1/CollapsableSearch/script.js
@@ -1,6 +1,8 @@
 let searchButton = document.getElementById('search-button')
 let rippleButton = document.getElementById('ripple-button')
 let searchContainer = document.getElementById('searchConatiner')
+let fill = document.querySelector('.fill')
+let empties = document.querySelectorAll('.empty')
 searchButton.addEventListener('click' , () => {
     searchContainer.classList.toggle('active')
 })
@@ -16,3 +18,53 @@ rippleButton.addEventListener('click',function (e){
     // },1000)
     
 })
+
+fill.addEventListener('dragstart',dragStart)
+fill.addEventListener('dragend',dragEnd)
+
+empties.forEach(empty => {
+    empty.addEventListener('dragover',dragOver)
+    empty.addEventListener('dragenter',dragEnter)
+    empty.addEventListener('dragleave',dragLeave)
+    empty.addEventListener('drop',dragDrop)
+})
+
+function dragStart() {
+    console.log('Dragging started')
+    this.className += ' hold';
+    setTimeout(()=>{
+        this.className = 'invisible'
+    })
+}
+function dragEnd() {
+    console.log('Dragging Ended')
+    this.className = 'fill';
+}
+function dragOver(e) {
+    e.preventDefault();
+    console.log('Dragging Over')
+}
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered'; 
+    console.log('Dragging Entered')
+}
+function dragLeave() {
+    console.log('Dragging left')
+    this.className = 'empty'; 
+}
+function dragDrop() {
+    this.className = 'empty'
+    console.log('dropped')
+    this.appendChild(fill)
+
+}
+
+// there are 6 types of drag events 
+
+// dragstart -- it fires when the draggable element is dragged
+// dragend -- fires when the dragging ends
+// dragenter -- fires when 
+// dragleave
+// dragover
+// drop
