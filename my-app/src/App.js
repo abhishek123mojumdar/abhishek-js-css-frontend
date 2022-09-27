a66ada50a0c7bc2bee0989b749cd73dceb7236bc
Mojumdar <Abhishek.Mojumdar@dell.com>
2022-09-27 21:38:56 +0530

    react app dev vs-code

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FrontEndComponent from './FrontEnd/FrontEndComponent'
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Let us learn React</h1>
+       <FrontEndComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/FrontEnd/FrontEndComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FrontEndComponent() {
+  return (
+    <div>
+       <h1>This is a React Component</h1>
+       <button>Click me</button>
+    </div>
+  );
+}
+
+export default FrontEndComponent;
